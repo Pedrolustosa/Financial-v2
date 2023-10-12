a36fe9fa915f983217b0ecc716c10b918de3dcc9
Pedrolustosa <pedroeternalss@gmail.com>
2023-10-12 20:57:02 -0300

    feat: Creating Request Interceptor/Wheel Guard - Lesson 8

--- Front/Financial_System/src/app/app-routing.module.ts
+++ Front/Financial_System/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { AuthGuard } from './pages/guards/auth-guard.service';
 
 const routes: Routes = [
   {
@@ -16,15 +17,19 @@ const routes: Routes = [
   },
   {
     path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(dm => dm.DashboardModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'system', loadChildren: () => import('./pages/system/system.module').then(sm => sm.SystemModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'category', loadChildren: () => import('./pages/category/category.module').then(cm => cm.CategoryModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'expenditure', loadChildren: () => import('./pages/expenditure/expenditure.module').then(em => em.ExpenditureModule),
+    canActivate: [AuthGuard]
   }
 ];
 
---
+++ Front/Financial_System/src/app/services/auth.service.ts
@@ -0,0 +1,51 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+  private usuarioAutenticadoPortal: boolean = false;
+  private token: any;
+  private user: any;
+
+  constructor(private httpClient: HttpClient) { }
+
+  checkToken() {
+    return Promise.resolve(true);
+  }
+
+  AuthenticatedUser(status: boolean) {
+    localStorage.setItem('usuarioAutenticadoPortal', JSON.stringify(status));
+    this.usuarioAutenticadoPortal = status;
+  }
+
+  UserIsAuthenticated(): Promise<boolean> {
+    this.usuarioAutenticadoPortal = localStorage.getItem('usuarioAutenticadoPortal') == 'true';
+    return Promise.resolve(this.usuarioAutenticadoPortal);
+  }
+
+  setToken(token: string) {
+    localStorage.setItem('token', token);
+    this.token = token;
+  }
+
+  get getToken() {
+    this.token = localStorage.getItem('token');
+    return this.token;
+  }
+
+  ClearToken() {
+    this.token = null;
+    this.user = null;
+  }
+
+  ClearDataUser() {
+    this.AuthenticatedUser(false);
+    this.ClearToken();
+    localStorage.clear();
+    sessionStorage.clear();
+  }
+}
